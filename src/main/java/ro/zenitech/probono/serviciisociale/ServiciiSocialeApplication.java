296c00b3395afd3d23d2199c43cabc2cf3e07361
greeneye256 <radusimu@gmail.com>
2020-04-16 18:00:20 +0300

    v1

---
+++ src/main/java/ro/zenitech/probono/serviciisociale/ServiciiSocialeApplication.java
@@ -0,0 +1,13 @@
+package ro.zenitech.probono.serviciisociale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciiSocialeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiciiSocialeApplication.class, args);
+    }
+
+}
